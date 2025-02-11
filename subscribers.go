5015e52c53100761d762569f17974ddda0e4fff7
Arnoud Vermeer <a.vermeer@freshway.biz>
2025-02-11 13:57:07 +0100

    Add username regex parsing to ignore @dhcpv4 or @dhcpv6 domains

--- subscribers.go
+++ subscribers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"regexp"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Subscriber struct {
@@ -32,10 +36,30 @@ func loadSubscribers(filename string) ([]Subscriber, error) {
 
 // Find a subscriber by identifier
 func findSubscriber(subscribers []Subscriber, identifier string) *Subscriber {
+	username := ""
+	res, err := ParseUsername(identifier)
+	if err != nil {
+		log.Errorf("Username parsing error: %v", err)
+	} else {
+		username = res[0]
+	}
+
 	for _, sub := range subscribers {
-		if sub.Identifier == identifier {
+		if sub.Identifier == username {
 			return &sub
 		}
 	}
 	return nil
+}
+
+func ParseUsername(username string) ([]string, error) {
+	// Regex vallejo.ps1:22-23@dhcpv4
+	validUsername := regexp.MustCompile(`(?P<routerID>[\w]+)\.(?P<intf>[\w\/-]+)\:(?:(?P<svlanID>\d{1,4})-)?(?P<cvlanID>\d{1,4})`)
+	matches := validUsername.FindStringSubmatch(username)
+	if len(matches) == 0 {
+		return nil, fmt.Errorf("could not match username with regex: %v", username)
+	} else if len(matches) != len(validUsername.SubexpNames()) {
+		return nil, fmt.Errorf("could not fully parse username: %v, only matched on %d out of %d", username, len(matches), len(validUsername.SubexpNames()))
+	}
+	return matches, nil
 }
